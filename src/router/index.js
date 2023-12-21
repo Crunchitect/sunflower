aa57eb0864b7dd70bbb2240a9b7e208cb42f7d9f
PremPremPremPrem <prem.sy1952@gmail.com>
2023-12-21 23:02:10 +0700

    sunflower

---
+++ src/router/index.js
@@ -0,0 +1,13 @@
+import { createRouter, createWebHistory } from 'vue-router'
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      name: "Home",
+      path: "/",
+      component: () => import("../views/Intro.vue")
+    }
+  ]
+})
+
+export default router
---
+++ src/views/Intro.vue
@@ -0,0 +1,45 @@
+<script setup>
+    import anime from "../lib/anime";
+    import { ref, onMounted } from 'vue';
+    import Handwriting from '../components/icons/Handwriting.vue';
+
+    const sleep = ms => new Promise(r => setTimeout(r, ms));
+    const lin_grad = `linear-gradient(white 0.1%, #f7ff00 10%, #ff9900 60%)`
+
+    onMounted(async () => {
+        anime({
+            targets: '.intro svg path',
+            strokeDashoffset: [anime.setDashoffset, 0],
+            easing: 'easeInOutSine',
+            duration: 3500,
+            delay: anime.stagger(150),
+        });
+        await sleep(3500);
+        document.querySelector('.intro svg path')
+            .attributes
+            .stroke
+            .value = "white";
+        document.querySelector('.intro').style.background = lin_grad;
+        document.querySelector('.intro').style.backgroundPosition = '1000%';
+    });
+</script>
+
+<template>
+    <div class="intro">
+    <Handwriting class="handwriting" />
+    </div>
+</template>
+
+<style scoped>
+  .intro {
+    width: 100dvw;
+    height: 100dvh;
+    display: flex;
+    flex-flow: row wrap;
+    place-content: center;
+  }
+
+  .handwriting {
+    transform: scale(1.8);
+  }
+</style>
